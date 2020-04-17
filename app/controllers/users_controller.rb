bfe467161e5bc46be48bca8f37ddad9b42b9052a
rachelemmer <rachelaemmer@gmail.com>
2020-04-17 09:01:39 -0600

    Add custom route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
 
+    before_action :authorize, only: [:favorite_books]
+
     def index 
         @users = User.all 
         render json: @users
     end 
+
+    def favorite_books
+        user = User.find(@user_id)
+        render json: user.books
+    end
 
     def create
         @user = User.create(
